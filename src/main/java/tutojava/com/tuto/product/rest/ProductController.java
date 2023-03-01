5de249b402c54abacbdbaf818b27d3e3324baa93
8303244R <xavier.lancelle@sncf.fr>
2023-03-01 09:59:49 +0100

    add javadoc

--- src/main/java/tutojava/com/tuto/product/rest/ProductController.java
+++ src/main/java/tutojava/com/tuto/product/rest/ProductController.java
@@ -22,27 +22,49 @@ public class ProductController {
     @Autowired
     private ProductServiceImpl productServiceImpl;
 
+    /**
+     * 
+     * @return Une liste {@link ProductDto} correspondant à l'ensemble des produits
+     */
     @GetMapping
     public List<ProductDto> getProducts() {
         return productServiceImpl.findAll();
     }
 
+    /**
+     * 
+     * @param id L'identifiant du produit à lire
+     * @return Un {@link ProductDto} représentant le produit demandé
+     */
     @GetMapping("/{id}")
     public ProductDto getProductById(@PathVariable int id) {
         return productServiceImpl.findById(id);
     }
 
+    /**
+     * 
+     * @param product un {@link ProductDto}
+     */
     @PostMapping
     public void add(@RequestBody ProductDto product) {
         productServiceImpl.add(product);
     }
 
+    /**
+     * 
+     * @param id L'identifiant du produit que l'on veux modifier
+     * @param product un {@link ProductDto}
+     */
     @PutMapping("/{id}")
     public void updateProduct(@PathVariable int id, @RequestBody ProductDto product) {
         product.setId(id);
         productServiceImpl.update(product);
     }
     
+    /**
+     * 
+     * @param id L'identifiant du produit que l'on veux supprimer
+     */
     @DeleteMapping("/{id}")
     public void delete(@PathVariable int id) {
         productServiceImpl.delete(id);
---
+++ src/main/java/tutojava/com/tuto/shared/database/SecretManager.java
@@ -0,0 +1,9 @@
+package tutojava.com.tuto.shared.database;
+import lombok.Getter;
+
+@Getter
+public class SecretManager {
+    private String username = "xavilanc";
+    private String password = "tichot";
+
+}
